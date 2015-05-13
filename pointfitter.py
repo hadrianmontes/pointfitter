205ea4f874315b4b3cc3570add42cabfb80c5264
Hadrián Montes <hadrianmontes@gmail.com>
2015-05-14 01:43:23 +0200

    Added trigonometrical arc functions

--- pointfitter.py
+++ pointfitter.py
@@ -15,7 +15,7 @@ from scipy.interpolate import UnivariateSpline
 from scipy import stats
 #import scipy.special._ufuncs_cxx
 import numpy as np
-from numpy import e,pi,exp,cos,sin,tan,log,log10
+from numpy import e,pi,exp,cos,sin,tan,log,log10,arctan,arccos,arcsin
 import tkMessageBox,tkFileDialog
 #import FileDialog
 
